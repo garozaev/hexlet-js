ea50e5de7df574cb38029fb116840091e8d15753
Georgy Rozaev <g.a.rozaev@gmail.com>
2024-08-18 22:07:25 +0300

    update index.js package.json package-lock.json src/index.js after add eslint

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+// eslint-disable-next-line import/extensions
 import hello from './src/index.js';
+
 hello();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import _ from "lodash";
+import _ from 'lodash';
+
 const hello = () => {
     console.log('Hello, Hexlet!');
     console.log(_.last(['one', 'two']));
 };
+
 export default hello;
